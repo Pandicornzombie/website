fdbd6a56c5ae5427e7a1e0078627afb7b04ccb06
Iulian Onofrei <iulian.onofrei@yahoo.com>
2021-02-10 00:29:04 +0200

    Added @nuxtjs/tailwindcss package version 3.4.2

--- nuxt.config.js
+++ nuxt.config.js
@@ -49,7 +49,9 @@ export default {
     // https://go.nuxtjs.dev/typescript
     '@nuxt/typescript-build',
     // https://go.nuxtjs.dev/stylelint
-    '@nuxtjs/stylelint-module'
+    '@nuxtjs/stylelint-module',
+    // https://go.nuxtjs.dev/tailwindcss
+    '@nuxtjs/tailwindcss'
   ],
 
   // Modules: https://go.nuxtjs.dev/config-modules
--- pages/index.vue
+++ pages/index.vue
@@ -34,6 +34,11 @@ export default Vue.extend({})
 </script>
 
 <style>
+/* Sample `apply` at-rules with Tailwind CSS
+.container {
+@apply min-h-screen flex justify-center items-center text-center mx-auto;
+}
+*/
 .container {
   margin: 0 auto;
   min-height: 100vh;
