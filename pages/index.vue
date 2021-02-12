7e7d218781a95d9c9e264d6d2bec42cd18739045
Iulian Onofrei <iulian.onofrei@yahoo.com>
2021-02-12 22:02:54 +0200

    Updated the drawings layout

--- pages/index.vue
+++ pages/index.vue
@@ -1,13 +1,13 @@
 <template>
-  <div class="container mx-auto">
+  <div>
     <h1>Drawings</h1>
-    <div class="grid-flow-col">
-      <div v-for="drawing of drawings" :key="drawing.slug" class="inline-block h-72">
+    <div class="col-count-1 sm:col-count-2 md:col-count-3 xl:col-count-4 gap-x-4 max-w-max mx-auto">
+      <div v-for="drawing of drawings" :key="drawing.slug" class="max-w-max mb-4">
         <NuxtLink :to="{ name: 'drawing-slug', params: { slug: drawing.slug } }">
           <h2 v-if="drawing.title">
             {{ drawing.title }}
           </h2>
-          <img :src="drawing.img" class="h-full">
+          <img :src="drawing.img" class="rounded-lg">
         </NuxtLink>
       </div>
     </div>
---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  theme: {
+    columnCount: [1, 2, 3, 4]
+  },
+  plugins: [
+    require('tailwindcss-multi-column')()
+  ]
+}
