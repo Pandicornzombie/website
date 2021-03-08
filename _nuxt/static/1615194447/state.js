dbc4f9221d5a05215d04d4615421427c8b911072
revolter <6d0847b9@opayq.com>
2021-03-08 09:07:32 +0000

    Deploying to gh-pages from  @ 679886da3ebcdec4b5da6e4d585f6145153d3130 🚀

---
+++ _nuxt/static/1615194447/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1615194447",layout:"default",error:a,serverRendered:true,routePath:b,config:{app:{basePath:b,assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"84e8bb18"}}}}(null,"\u002F"));
